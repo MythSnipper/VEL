91c41e8ed59227c788710e4cb422d016f38c05f1
MythSnipper <mythsnipper2022@gmail.com>
2025-11-21 18:31:12 +0200

    VEL

--- include/lexer.hpp
+++ include/lexer.hpp
@@ -6,22 +6,69 @@
 #include <string>
 #include <vector>
 
-
 enum class TokenType{
-    Function,
-    Typename,
-    Identifier,
-    Block,
-    Statement,
-    LeftParenthesis,
-    RightParenthesis,
-    LeftBracket,
-    RightBracket,
+    //keywords
+    INT_KEYWORD,
+    CHAR_KEYWORD,
+    DOUBLE_KEYWORD,
+    IF_KEYWORD,
+    ELSE_KEYWORD,
+    WHILE_KEYWORD,
+    FOR_KEYWORD,
+    RETURN_KEYWORD,
+
+    // Identifiers and literals
+    IDENTIFIER,
+    INT_LITERAL,
+    STRING_LITERAL,
+    CHAR_LITERAL,
+
+    // Operators
+
+    //assignment
+    ASSIGN,     // =
+    ADD_ASSIGN, // +=
+    SUB_ASSIGN, // -=
+    MUL_ASSIGN, // *=
+    DIV_ASSIGN, // /=
+    MOD_ASSIGN, // %=
+    XOR_ASSIGN, // ^=
+
+    //binary
+    ADD,        // +
+    SUB,        // -
+    MUL,        // *
+    DIV,        // /
+    MOD,        // %
+    LT,         // <
+    GT,         // >
+    LTE,        // <=
+    GTE,        // >=
+    NEQ,        // !=
+    AND,        // &
+    ANDAND,     // &&
+    OR,         // |
+    OROR,       // ||
+
+    NOT,        // !
+    
+    EQ,         // ==
+
+    // Punctuation
+    SEMICOLON,  // ;
+    COMMA,      // ,
+    LPAREN,     // (
+    RPAREN,     // )
+    LBRACE,     // {
+    RBRACE,     // }
+
+    // Special
+    END_OF_FILE,
 };
 
 struct Token{
     TokenType type;
-    std::string text;
+
 };
 
 
